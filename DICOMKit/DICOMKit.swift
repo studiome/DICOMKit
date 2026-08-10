f478199ee7cdbe81d42e0afc4e7cae5ccdf8f7dc
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2026-08-10 10:47:16 +0900

    Initial Commit

---
+++ DICOMKit/DICOMKit.swift
@@ -0,0 +1,9 @@
+//
+//  DICOMKit.swift
+//  DICOMKit
+//
+//  Created by kmiyahara on 2026/08/10.
+//
+
+import Foundation
+
---
+++ DICOMKitTests/DICOMKitTests.swift
@@ -0,0 +1,19 @@
+//
+//  DICOMKitTests.swift
+//  DICOMKitTests
+//
+//  Created by kmiyahara on 2026/08/10.
+//
+
+import Testing
+@testable import DICOMKit
+
+struct DICOMKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
