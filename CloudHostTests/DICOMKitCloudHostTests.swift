39ef4901959d634f80397e11bd40c6d3251bd790
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2026-08-10 15:57:48 +0900

    Make CI SwiftPM-only

--- CloudHostTests/DICOMKitCloudHostTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-@testable import DICOMKitCloudHost
-
-struct DICOMKitCloudHostTests {
-    @Test func linksLocalDICOMKitPackage() {
-        #expect(DICOMKitCloudHost.explicitVRLittleEndianUID == "1.2.840.10008.1.2.1")
-    }
-}
